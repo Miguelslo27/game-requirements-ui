1107fc057c67391936f238444c433f557cf5bd53
Miguel <miguelmail2006@gmail.com>
2023-03-19 20:43:07 -0300

    feature/game-details - Improve code to be more declarative

--- src/pages/api/games/index.tsx
+++ src/pages/api/games/index.tsx
@@ -1,51 +1,51 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 import cheerio from 'cheerio';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed.' });
-  }
-  
-  // >>> GET TOKEN
-  const clientId = '1kr7nti016horcw8y2cfajed6a12rv';
-  const clientSecret = 'l3p9azgiausqd7gspyork5blkbi5jg';
+const getClientCredentials = async () => {
+  const clientId = process.env.TWITCH_CLIENT_ID;
+  const clientSecret = process.env.TWITCH_CLIENT_SECRET;
   const grantType = 'client_credentials';
   const params = new URLSearchParams({
-    client_id: clientId,
-    client_secret: clientSecret,
-    grant_type: grantType
+    client_id: clientId || '',
+    client_secret: clientSecret || '',
+    grant_type: grantType,
   });
-  let accessToken;
-  
+
   try {
-    const responseToken = await axios.post('https://id.twitch.tv/oauth2/token', params);
-    accessToken = responseToken.data.access_token;
+    const responseToken = await axios.post(
+      'https://id.twitch.tv/oauth2/token',
+      params
+    );
+    return responseToken.data.access_token;
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: 'Internal server error.' });
+    return null;
   }
-  // console.log('TOKEN', accessToken);
-  // <<< GET TOKEN
-  
-  const { slug } = req.body;
-  const api = 'https://api.igdb.com/v4';
-  
-  try {
-    const config = {
-      headers: {
-        'Client-ID': clientId,
-        'Authorization': `Bearer ${accessToken}`,
-        'Content-Type': 'text/plain',
-      },
-    };
-    const responseForGame = await axios.post(`${api}/games`, `
-      fields *; where slug = "${slug}";
-    `, { ...config });
-    const game = responseForGame.data[0];
+};
 
-    // MULTI QUERY UP TO 10
-    const responseForMultiQuery = await axios.post(`${api}/multiquery`, `
+const getGameData = async (slug: string, accessToken: string) => {
+  const clientId = process.env.TWITCH_CLIENT_ID;
+  const config = {
+    headers: {
+      'Client-ID': clientId,
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'text/plain',
+    },
+  };
+  const api = 'https://api.igdb.com/v4';
+  const responseForGame = await axios.post(
+    `${api}/games`,
+    `
+      fields *; where slug = "${slug}";
+    `,
+    { ...config }
+  );
+  const game = responseForGame.data[0];
+  // MULTI QUERY UP TO 10
+  const responseForMultiQuery = await axios.post(
+    `${api}/multiquery`,
+    `
       query covers "covers" {
         fields *;
         where game=${game.id};
@@ -76,7 +76,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       };
       query screenshots "screenshots" {
         fields *;
-        where id=(${game.screenshots.join(',')}); 
+        where id=(${game.screenshots.join(',')});
       };
       query games "similar_games" {
         fields *;
@@ -86,9 +86,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         fields *;
         where id=(${game.websites.join(',')});
       };
-    `, { ...config });
-
-    const responseForMultiQuery2 = await axios.post(`${api}/multiquery`, `
+    `,
+    { ...config }
+  );
+  const responseForMultiQuery2 = await axios.post(
+    `${api}/multiquery`,
+    `
       query alternative_names "alternative_names" {
         fields *;
         where game=${game.id};
@@ -117,46 +120,79 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         fields *;
         where platforms=(${game.platforms.join(',')});
       };
-    `, { ...config });
+    `,
+    { ...config }
+  );
+  responseForMultiQuery.data.forEach((data: any) => {
+    game[data.name] = data.result;
+  });
+  responseForMultiQuery2.data.forEach((data: any) => {
+    game[data.name] = data.result;
+  });
 
-    responseForMultiQuery.data.forEach((data: any) => {
-      game[data.name] = data.result;
-    });
-    responseForMultiQuery2.data.forEach((data: any) => {
-      game[data.name] = data.result;
-    });
-  
-    const websiteResponse = await axios.get(
-      game.websites.filter(web => web.url.includes('store.steampowered.com'))[0].url,
-    );
-    const html = websiteResponse.data;
-    const $ = cheerio.load(html);
-    const requirements = {};
+  return game;
+};
 
-    $('.sysreq_tab').each((index, content) => {
-      const system = $(content).text().trim();
-      requirements[system] = {};
+const getRequirements = async (game: any) => {
+  const websiteResponse = await axios.get(
+    game.websites.filter((web: any) => web.url.includes('store.steampowered.com'))[0]
+      .url
+  );
+  const html = websiteResponse.data;
+  const $ = cheerio.load(html);
+  const requirements = {} as any;
 
-      $(`.sysreq_content:nth(${index}) > div`).each((reqsIndex, reqsContent) => {
-        const levelSysRequirements = $(reqsContent).find('> ul > strong').text().trim();
+  $('.sysreq_tab').each((index, content) => {
+    const system = $(content).text().trim();
+    requirements[system] = {};
 
-        if (levelSysRequirements) {
-          requirements[system][levelSysRequirements] = [];
-  
-          $(reqsContent).find('> ul > ul > li').each((reqIndex, reqContent) => {
-            const reqHardwareType = $(reqContent).find('strong').text().trim();
-            const reqHardwareSpecs = $(reqContent).html().replace(`<strong>${reqHardwareType}</strong>`, '').replace('<br>', '').trim()
-  
-            requirements[system][levelSysRequirements].push({
-              hardware: reqHardwareType,
-              specs: reqHardwareSpecs,
-            });
+    $(`.sysreq_content:nth(${index}) > div`).each((reqsIndex, reqsContent) => {
+      const levelSysRequirements = $(reqsContent).find('> ul > strong').text().trim();
+
+      if (levelSysRequirements) {
+        requirements[system][levelSysRequirements] = [];
+
+        $(reqsContent).find('> ul > ul > li').each((reqIndex, reqContent) => {
+          const reqHardwareType = $(reqContent).find('strong').text().trim();
+          const reqContentHtml = $(reqContent).html();
+          const reqHardwareSpecs = reqContentHtml
+            ? reqContentHtml
+                .replace(`<strong>${reqHardwareType}</strong>`, '')
+                .replace('<br>', '')
+                .trim()
+            : '';
+
+          requirements[system][levelSysRequirements].push({
+            hardware: reqHardwareType,
+            specs: reqHardwareSpecs,
           });
-        }
-      });
+        });
+      }
     });
+  });
 
+  return requirements;
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed.' });
+  }
+
+  const accessToken = await getClientCredentials();
+
+  console.log('ACCESS TOKEN', accessToken);
+
+  if (accessToken === null) {
+    return res.status(500).json({ message: 'Internal server error.' });
+  }
+
+  try {
+    const { slug } = req.body;
+    const game = await getGameData(slug, accessToken);
+    const requirements = await getRequirements(game);
     game.requirements = requirements;
+
     return res.status(200).json(game);
   } catch (error) {
     console.error('Error', error);
